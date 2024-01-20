63fcd56163ce11f95e76d62d310557da7e2dceb8
mwangiowen <owen31588@gmail.com>
2024-01-20 23:48:19 +0300

    build(deps): update Pipfile, Pipfile.lock and nyc_api.py

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,29 @@
-
 import requests
 import json
 
+
 class GetPrograms:
+    def get_programs(self):
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        response = requests.get(URL)
+        return response.text  # Use response.text instead of response.content
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def program_school(self):
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            if "agency" in program:
+                programs_list.append(program["agency"])
 
-    response = requests.get(URL)
-    return response.content
+        return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+
+# Instantiate GetPrograms class
+programs_instance = GetPrograms()
+
+# Call program_school method
+programs_schools = programs_instance.program_school()
+
+# Print unique schools
+for school in set(programs_schools):
+    print(school)
